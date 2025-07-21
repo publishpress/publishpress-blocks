dfe61c40745405d6cae0cb48d3ae29a39f6f1575
Olawale Adesina <olawale.olatech.pro@gmail.com>
2025-07-21 09:14:32 +0100

    Fix #1549 #1550 #1548

--- incl/utilities-main.php
+++ incl/utilities-main.php
@@ -58,7 +58,7 @@ if (! class_exists('\\PublishPress\\Blocks\\Utilities')) {
                 'name' => 'advgb/feature',
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>',
                 'title' => __('Feature', 'advanced-gutenberg'),
-                'description' => __('Show your visitors a list of features. This is ideal for pricing and comparison tables.', 'advanced-gutenberg'),
+                'description' => __('Use block to create a list inside the Feature List" block.', 'advanced-gutenberg'),
                 'category' => 'advgb-category',
                 'parent' => ['advgb/feature-list'],
                 'iconColor' => '#655997',
@@ -69,7 +69,7 @@ if (! class_exists('\\PublishPress\\Blocks\\Utilities')) {
                 'name' => 'advgb/feature-list',
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"></rect></g><g><g><path d="M20,3H4C2.9,3,2,3.9,2,5v14c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V5 C22,3.9,21.1,3,20,3z M20,19H4V5h16V19z" fill-rule="evenodd"></path><polygon fill-rule="evenodd" points="19.41,10.42 17.99,9 14.82,12.17 13.41,10.75 12,12.16 14.82,15"></polygon><rect fill-rule="evenodd" height="2" width="5" x="5" y="7"></rect><rect fill-rule="evenodd" height="2" width="5" x="5" y="11"></rect><rect fill-rule="evenodd" height="2" width="5" x="5" y="15"></rect></g></g></svg>',
                 'title' => __('Feature List', 'advanced-gutenberg'),
-                'description' => __('Show your visitors a list of features. This is ideal for pricing and comparison tables.', 'advanced-gutenberg'),
+                'description' => __('Use block to create a list inside the Feature List" block.', 'advanced-gutenberg'),
                 'category' => 'advgb-category',
                 'iconColor' => '#655997',
                 'isPro' => true
