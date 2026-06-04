474850a94b5185f1c079a13c6eaba626d4a70b3c
olatechpro <olawale.olatech.pro@gmail.com>
2026-06-04 17:43:41 +0100

    Compile translation

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ab58c05d0c9f200b8d2b16e86bde4b5b8a37c65c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ab58c05d0c9f200b8d2b16e86bde4b5b8a37c65c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
